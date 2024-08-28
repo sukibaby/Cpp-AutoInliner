ec63703f5dfa2973335686d701a5e4bad3ff8e38
sukibaby <163092272+sukibaby@users.noreply.github.com>
2024-08-28 04:17:34 -0700

    Add program and README

---
+++ Cpp-AutoInliner.h
@@ -0,0 +1,12 @@
+﻿// The program is self-contained in the .cpp, but I'd rather have this here in case I need to use it later.
+
+#ifndef CPP_AUTO_INLINER_H
+#define CPP_AUTO_INLINER_H
+
+#include <string>
+
+std::string readFile(const std::string& absoluteFilePath);
+void writeFile(const std::string& absoluteFilePath, const std::string& fileContent);
+std::string inlineMacros(const std::string& fileContent);
+
+#endif // CPP_AUTO_INLINER_H
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Cpp-AutoInliner
  A tool to automatically inline macros in a given .cpp / .h file.
+ I made it to help with my workflow, but also because I wanted to see what the compiler might see after inlining but before compiling.
